61e89e773ec13148111e998078d88cbdee104179
Janette Rocha <113930409+jrocha26@users.noreply.github.com>
2022-09-21 17:37:34 +0000

    made a dictionary

--- main.py
+++ main.py
@@ -129,13 +129,25 @@ print(my_result)
 
 # dictionaries
 # another data structure that stores data that can be unstructured
-# employee = {'first_name': 'steven',
-#             'last_name': 'pretti',
-#              'weight': 37.2,
-#               'size': 5.77
-#              'height:' 6}
-# print(employee['weight'])
+employee = {'first_name': 'steven',
+             'last_name': 'pretti',
+              'weight': 37.2,
+               'size': 5.77,
+              'height': 6}
+print(employee['weight'])
+print(employee['height'])
 
+warrior = {'made_by': 'hbomax',
+           'release_date': 2018,
+           'budget': '24million',
+           'rotten_tomatoes_score':'fresh'}
+print(warrior['release_date'])
+print(warrior['budget'])
+#how to add to ditionaries 
+warrior['score'] = 78
+print(warrior.items())
+print(warrior.keys())
+print(warrior.values())
 #challenge 1
 # Create a dictionary called my_dict that stores the following information about a person:
 # name: Karen
@@ -143,7 +155,10 @@ print(my_result)
 # age: 35
 # occupation: Journalist
 # The names of the keys and values must be equal to the ones indicated above.
-
+my_dict = {'name': 'Karen',
+          'surname':'Jurgens',
+          'age':35,
+          'occupation':'Journalist'}
 #challenge 2:
 # Use print to returns the second item of the list called points2, inside the following dictionary.
 
